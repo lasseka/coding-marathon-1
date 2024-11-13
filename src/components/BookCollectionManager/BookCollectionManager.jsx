6eb467389be03bc3e00c05adb71b052646a4d4b9
vevego <veera.ruotsalainen@metropolia.fi>
2024-11-13 16:30:32 +0200

    self assesment added

--- src/components/BookCollectionManager/BookCollectionManager.jsx
+++ src/components/BookCollectionManager/BookCollectionManager.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import "./BookCollectionManager.css"
 import Book from "./Book"
 
+
 function BookCollectionManager() {
 
+  // I decided to keep this part simple
   const [books, setBooks] = useState([]);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
@@ -11,6 +13,7 @@ function BookCollectionManager() {
 
 
   // Add a new book to the list
+  // I first did few mistakes with the setBooks function as the Task template did the setting a bit differently.
   function addBook() {
     if (title.trim() !== "" && author.trim() !== "" && year.trim() !== "") {
       setBooks((b) => [...b, { title, author, year }]);
@@ -21,11 +24,14 @@ function BookCollectionManager() {
   }
 
   // Delete a book from the list
+  // This was tricky for me to understand at first.
   function deleteBook(index) {
     const updatedBooks = books.filter((_, i) => i !== index);
     setBooks(updatedBooks); // Update the state with the new list
   }
 
+  // I had few errors with the mapping of the books after refactoring. Luckily it was easy to fix. The onChanges in inputs were easy as I just did those on pair programming.
+  // I could have named the classes better and think about the css as I was writing this part and not afterwards.
   return (
     <div className="book-collection-manager">
       <h1>Book Collection Manager</h1>
