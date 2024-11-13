9bc1b172c115b0a1b34088e43dfa622c26706e42
silpps <hilda.hermunen@gmail.com>
2024-11-13 14:55:02 +0200

    Email and password input boxes have border color depending on users input. More css

--- src/components/SignupPage/SignupPage.jsx
+++ src/components/SignupPage/SignupPage.jsx
@@ -5,18 +5,28 @@ import './SignUpPage.css';
 const SignUp = () => {
     const [nationality, setNationality] = useState('');
     const [email, setEmail] = useState('');
+    const [emailValid, setEmailValid] = useState(false);
     const [password, setPassword] = useState('');
+    const [strongPassword, setStrongPassword] = useState(false);
     const handleNationalityChange = (e) => {
         setNationality(e.target.value);
     }
     const [greeting, setGreeting] = useState('');
 
     const handleEmailChange = (e) => {
-        setEmail(e.target.value);
+        const inputEmail = e.target.value;
+        setEmail(inputEmail);
+        console.log(emailValid);
+        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        setEmailValid(emailRegex.test(inputEmail));
     }
 
     const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
+      const inputPassword = e.target.value;
+        setPassword(inputPassword);
+        console.log(strongPassword);
+        const passwordRegex = /(?=.*[a-z])+(?=.*[A-Z])+(?=.*[0-9])+(?=.*[\W_])+[\S]{8,}/;
+        setStrongPassword(passwordRegex.test(inputPassword));   
     }
 
     const handleSubmit = (e) => {
@@ -47,21 +57,28 @@ const SignUp = () => {
                 placeholder="Enter your email"
                 value={email}
                 onChange ={handleEmailChange}
+                className={emailValid ? 'valid' : 'invalid'}
                 required/>
+                {!emailValid && <p className="error-message">Please enter a valid email address</p>}
+                {emailValid && <p className="success-message">Email address is valid</p>}
                 <p className="description">Password</p>
                 <input
                  type="password" 
                  placeholder="Enter your password"
                  value= {password}
                  onChange = {handlePasswordChange}
+                 className = {strongPassword ? 'valid' : 'invalid'}
                  required />
+                 {!strongPassword && <p className="error-message">Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character</p>}
+                 {strongPassword && <p className="success-message">Password is strong</p>}
                 <p className="description">Nationality</p>
                 <select
                 id = "nationality"
                 value= {nationality}
                 onChange ={handleNationalityChange}
+                required
                 >
-                    <option value="" disabled selected>
+                    <option value="" disabled>
                     Select nationality
                     </option>
                     <option value = "fi">Finnish</option>
@@ -71,8 +88,8 @@ const SignUp = () => {
                 </select>
                 <button type="submit">Sign Up</button>
             </form>
-            {greeting && <p>{greeting}</p>}
-            {greeting && <p>Your email address is {email}</p>}
+            {greeting && <p className="greeting">{greeting}</p>}
+            {greeting && <p className="greeting">Your email address is {email}</p>}
         </div>
     );
 }
